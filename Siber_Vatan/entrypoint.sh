62b9cb8491914eccc1fc92d183479914220fe31e
batuhanberktepe <tepebatuhanberk4@gmail.com>
2025-10-28 02:02:34 +0300

    Update entrypoint.sh

--- Siber_Vatan/entrypoint.sh
+++ Siber_Vatan/entrypoint.sh
@@ -1,10 +1,11 @@
-#!/bin/sh
+ 1. ADIM (EKSİK OLAN): ÖNCE KLASÖRÜN sahibini ve iznini ayarla
+# Apache (www-data) kullanıcısının bu klasöre yeni dosyalar (-wal, -shm) oluşturabilmesi gerekir.
+chown www-data:www-data $DB_DIR
+chmod 775 $DB_DIR
 
-# Apache kullanıcısına (www-data) SQLite veritabanı dosyası için sahiplik ver
-chown www-data:www-data otobus_bilet.db
-
-# Sahip ve grubun dosyayı okuyup yazabilmesi için izinleri ayarla
-chmod 664 otobus_bilet.db
+# 2. ADIM: SONRA DOSYANIN sahibini ve iznini ayarla
+chown www-data:www-data $DB_FILE
+chmod 664 $DB_FILE
 
 # Dockerfile'da belirtilen asıl komutu (apache2-foreground) çalıştır
 exec "$@"
